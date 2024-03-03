10250a1d221cdc0379d12d4d81bdf025033aac8f
HenriV <henri.viigim2e@gmail.com>
2024-03-03 17:22:31 +0200

    Add function that determines computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
+function getComputerChoice(){
+
+    let choice;
+    let random = Math.floor(Math.random() * 3);
+
+    switch(random){
+
+        case 0:
+            choice = "rock";
+            break;
+
+        case 1:
+            choice = "paper";
+            break;
+
+        case 2:
+            choice ="scissors";
+            break;
+    }
+
+    return choice;
+
+}
 
